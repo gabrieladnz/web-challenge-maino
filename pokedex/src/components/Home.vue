07cf5bcec8848cc58d4bdf9461ae1ff3d7e12e97
Gabriela <gadisa10@outlook.com>
2024-04-16 14:42:33 -0300

    🚚 chore: definição de rotas iniciais

---
+++ pokedex/src/components/Home.vue
@@ -0,0 +1,19 @@
+<!-- Template  -->
+<template>
+    <div>
+        <h1>Pokédex</h1>
+    </div>
+</template>
+
+<!-- Script  -->
+<script>
+export default {
+    name: "Home",
+    data() {
+        return {};
+    },
+};
+</script>
+
+<!-- Scss  -->
+<style scoped lang="scss"></style>
