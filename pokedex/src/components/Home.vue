e1bf14d1a33dc88bf5690e8f1678dfbf3fa939f3
Gabriela <gadisa10@outlook.com>
2024-04-17 14:09:06 -0300

    🎨 style: estruturação das classes scss e metodologia BEM

--- pokedex/src/components/Home.vue
+++ pokedex/src/components/Home.vue
@@ -1,9 +1,35 @@
 <!-- Template  -->
-<template>
+<template class="home">
     <div>
-        <h1>Pokédex {{ nome }}</h1>
+        <!-- Título  -->
+        <div class="home__titulo">
+            <h1>Pokédex</h1>
+            <img src="../assets/logo.png">
+        </div>
+        <section class="home__section">
+            <!-- Barra de pesquisa  -->
+            <div class="home__section__barra-pesquisa">
+                <input type="text">
+                <img src="../assets/icons/lupa.svg">
+            </div>
+            <!-- Conteúdo principal  -->
+            <div class="home__section-conteudo-principal">
+                <aside class="home__section-menu-lateral">
+    
+                </aside>
+                <!-- Cards  -->
+                <main class="home__section-cards">
+    
+                </main>
+            </div>
+        </section>
+        <!-- Footer  -->
+        <footer class="home__footer">
+
+        </footer>
     </div>
 </template>
+
 
 <!-- Script  -->
 <script src="../utils/home.ts"></script>
