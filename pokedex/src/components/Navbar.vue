cde312072b6ba83080d5d647085227dfbd66eea9
Gabriela <gadisa10@outlook.com>
2024-04-17 16:02:02 -0300

    ✨ feat: adição da funcionalidade de alteração manual do idioma na barra de navegação

--- pokedex/src/components/Navbar.vue
+++ pokedex/src/components/Navbar.vue
@@ -1,12 +1,27 @@
 <template>
-    <nav>
-        <router-link to="/">{{ $t('navbar.item1') }}</router-link> |
-        <router-link to="/sobre">{{ $t('navbar.item2') }}</router-link>
-    </nav>
+    <div>
+        <nav>
+            <router-link to="/">{{ $t('navbar.item1') }}</router-link> |
+            <router-link to="/sobre">{{ $t('navbar.item2') }}</router-link>
+        </nav>
+        <div>
+            <span @click="alterarIdioma('pt_br')">PT</span>
+            <span @click="alterarIdioma('en')">EN</span>
+            <span @click="alterarIdioma('es')">ES</span>
+        </div>
+    </div>
 </template>
 
 <script>
 export default {
-    name: 'Navbar'
+    name: 'Navbar',
+    methods: {
+        /**
+         * Método para alterar o idioma manulamente no aplicativo.
+         */
+        alterarIdioma(local) {
+            this.$i18n.locale = local;
+        }
+    }
 }
 </script>
