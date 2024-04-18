5dd1037d6491f02084dd3c37399450cdf29ef9fe
Gabriela <gadisa10@outlook.com>
2024-04-18 14:16:26 -0300

    ✅ chore: ajuste funções de request para a api

--- pokedex/src/api/ApiService.ts
+++ pokedex/src/api/ApiService.ts
@@ -1,22 +1,26 @@
 import axios, { AxiosResponse } from 'axios';
 import { Pokemon } from './ApiInterface';
 
-const apiURL: string = 'https://pokeapi.co/api/v2/';
+class ApiService {
+  private apiURL: string = 'https://pokeapi.co/api/v2/';
+  
+  public getListaPokemon = async (): Promise<Pokemon[]> => {
+    try {
+      const response: AxiosResponse<{ results: Pokemon[] }> = await axios.get(`${this.apiURL}pokemon?limit=10`);
+      return response.data.results;
+    } catch (error) {
+      throw new Error(`Erro ao buscar lista de Pokémon: ${error}`);
+    }
+  };
 
-export const getListaPokemon = async (): Promise<Pokemon[]> => {
-  try {
-    const response: AxiosResponse<{ results: Pokemon[] }> = await axios.get(`${apiURL}pokemon?limit=1000`);
-    return response.data.results;
-  } catch (error) {
-    throw new Error(`Erro ao buscar lista de Pokémon: ${error}`);
-  }
-};
+  public getDetalhesPokemon = async (nomePokemon: string): Promise<any> => {
+    try {
+      const response: AxiosResponse<any> = await axios.get(`${this.apiURL}pokemon/${nomePokemon}`);
+      return response.data;
+    } catch (error) {
+      throw new Error(`Erro ao buscar detalhes do Pokémon ${nomePokemon} : ${error}`);
+    }
+  };
+}
 
-export const getDetalhesPokemon = async (nomePokemon: string): Promise<any> => {
-  try {
-    const response: AxiosResponse<any> = await axios.get(`${apiURL}pokemon/${nomePokemon}`);
-    return response.data;
-  } catch (error) {
-    throw new Error(`Erro ao buscar detalhes do Pokémon ${nomePokemon} : ${error}`);
-  }
-};
+export default ApiService;
