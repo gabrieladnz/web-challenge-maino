f1826c1adbfad353046816d1656ebc671f10eaea
Gabriela Diniz <gadisa10@outlook.com>
2024-04-20 18:20:16 -0300

    ✨ feat: criação de requisições retornando dados dos pokemons e exportando funções

--- pokedex/src/utils/pokemoncard.ts
+++ pokedex/src/utils/pokemoncard.ts
@@ -1,67 +1,81 @@
 import { defineComponent } from 'vue';
 
 export default defineComponent({
-    name: 'PokemonCard',
-    props: {
-        index: Number,
-        name: String,
-        id: Number,
-        url: String,
-        types: {
-            type: Array,
-            default: () => []
-        },
-        photo: String
+  name: 'PokemonCard',
+  props: {
+    index: Number,
+    name: String,
+    id: Number,
+    url: String,
+    types: {
+      type: Array,
+      default: () => []
     },
-    data() {
-        return {
-        }
-    },
-    methods: {
-        getBackgroundColor(type: string) {
-            switch (type) {
-              case 'normal':
-                return '#c89e6b';
-              case 'grass':
-                return '#92d1b4';
-              case 'fire':
-                return '#fd7a7a';
-              case 'water':
-                return '#75bdfd';
-              case 'electric':
-                return '#f5e585';
-              case 'ice':
-                return '#98d5d7';
-              case 'ground':
-                return '#e7d060';
-              case 'flying':
-                return '#a98ff0';
-              case 'poison':
-                return '#ad7fae';
-              case 'fighting':
-                return '#cf423a';
-              case 'psychic':
-                return '#f65687';
-              case 'dark':
-                return '#382d26';
-              case 'rock':
-                return '#b5b8a7';
-              case 'bug':
-                return '#99c98b';
-              case 'ghost':
-                return '#6e5896';
-              case 'steel':
-                return '#b9b7cf';
-              case 'dragon':
-                return '#8b5dff';
-              case 'fairy':
-                return '#f8cfd5';
-              default:
-                return '#c89e6b';
-            }
-          },
-          navegarPokemonSelecionado() {
-            this.$router.push({name: 'PokemonSelecionado', params: { id: this.id, name: this.name}});
-          }
+    photo: String
+  },
+  data() {
+    return {
     }
+  },
+  methods: {
+    /**
+     * 
+     * @param type 
+     * @returns 
+     */
+    // Chame a função getBackgroundColor aqui
+    getBackgroundColor(type: string) {
+      return getBackgroundColor(type);
+    },
+    /**
+     * 
+     */
+    navegarPokemonSelecionado() {
+      const rota = this.$router.resolve({ name: 'PokemonSelecionado', params: { id: this.id, name: this.name } }).href;
+      window.open(rota, '_blank');
+    }
+  }
 });
+
+export function getBackgroundColor(type: string) {
+  switch (type) {
+    case 'normal':
+      return '#c89e6b';
+    case 'grass':
+      return '#92d1b4';
+    case 'fire':
+      return '#fd7a7a';
+    case 'water':
+      return '#75bdfd';
+    case 'electric':
+      return '#f5e585';
+    case 'ice':
+      return '#98d5d7';
+    case 'ground':
+      return '#e7d060';
+    case 'flying':
+      return '#a98ff0';
+    case 'poison':
+      return '#ad7fae';
+    case 'fighting':
+      return '#cf423a';
+    case 'psychic':
+      return '#f65687';
+    case 'dark':
+      return '#382d26';
+    case 'rock':
+      return '#b5b8a7';
+    case 'bug':
+      return '#99c98b';
+    case 'ghost':
+      return '#6e5896';
+    case 'steel':
+      return '#b9b7cf';
+    case 'dragon':
+      return '#8b5dff';
+    case 'fairy':
+      return '#f8cfd5';
+    default:
+      return '#c89e6b';
+  }
+}
